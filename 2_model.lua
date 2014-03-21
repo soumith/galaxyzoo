159a09b2ca22018148cb74547d33b55326a5bbd9
Soumith Chintala <soumith@gmail.com>
2014-03-21 19:11:53 -0400

    decision tree graph #3

--- 2_model.lua
+++ 2_model.lua
@@ -7,74 +7,96 @@ featuresOut = 2048
 
 -- classifier size
 classifierHidden = {100, 100}
+dropout_p = 0.5
 
 features = nn.Sequential()
 
 
-
 branch1 = nn.Sequential()
+branch1:add(nn.Dropout(dropout_p))
 branch1:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch1:add(nn.Threshold(0, 1e-6))
+branch1:add(nn.Dropout(dropout_p))
 branch1:add(nn.Linear(classifierHidden[1], 3))
 branch1:add(nn.Softmax())
 
 branch2 = nn.Sequential()
+branch2:add(nn.Dropout(dropout_p))
 branch2:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch2:add(nn.Threshold(0, 1e-6))
+branch2:add(nn.Dropout(dropout_p))
 branch2:add(nn.Linear(classifierHidden[1], 2))
 branch2:add(nn.Softmax())
 
 branch3 = nn.Sequential()
+branch3:add(nn.Dropout(dropout_p))
 branch3:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch3:add(nn.Threshold(0, 1e-6))
+branch3:add(nn.Dropout(dropout_p))
 branch3:add(nn.Linear(classifierHidden[1], 2))
 branch3:add(nn.Softmax())
 
 branch4 = nn.Sequential()
+branch4:add(nn.Dropout(dropout_p))
 branch4:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch4:add(nn.Threshold(0, 1e-6))
+branch4:add(nn.Dropout(dropout_p))
 branch4:add(nn.Linear(classifierHidden[1], 2))
 branch4:add(nn.Softmax())
 
 branch1 = nn.Sequential()
+branch5:add(nn.Dropout(dropout_p))
 branch5:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch5:add(nn.Threshold(0, 1e-6))
+branch5:add(nn.Dropout(dropout_p))
 branch5:add(nn.Linear(classifierHidden[1], 4))
 branch5:add(nn.Softmax())
 
 branch6 = nn.Sequential()
+branch6:add(nn.Dropout(dropout_p))
 branch6:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch6:add(nn.Threshold(0, 1e-6))
+branch6:add(nn.Dropout(dropout_p))
 branch6:add(nn.Linear(classifierHidden[1], 2))
 branch6:add(nn.Softmax())
 
 branch7 = nn.Sequential()
+branch7:add(nn.Dropout(dropout_p))
 branch7:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch7:add(nn.Threshold(0, 1e-6))
+branch7:add(nn.Dropout(dropout_p))
 branch7:add(nn.Linear(classifierHidden[1], 3))
 branch7:add(nn.Softmax())
 
 branch8 = nn.Sequential()
+branch8:add(nn.Dropout(dropout_p))
 branch8:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch8:add(nn.Threshold(0, 1e-6))
+branch8:add(nn.Dropout(dropout_p))
 branch8:add(nn.Linear(classifierHidden[1], 7))
 branch8:add(nn.Softmax())
 
 branch9 = nn.Sequential()
+branch9:add(nn.Dropout(dropout_p))
 branch9:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch9:add(nn.Threshold(0, 1e-6))
+branch9:add(nn.Dropout(dropout_p))
 branch9:add(nn.Linear(classifierHidden[1], 3))
 branch9:add(nn.Softmax())
 
 branch10 = nn.Sequential()
+branch10:add(nn.Dropout(dropout_p))
 branch10:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch10:add(nn.Threshold(0, 1e-6))
+branch10:add(nn.Dropout(dropout_p))
 branch10:add(nn.Linear(classifierHidden[1], 3))
 branch10:add(nn.Softmax())
 
 branch11 = nn.Sequential()
+branch11:add(nn.Dropout(dropout_p))
 branch11:add(nn.Linear(featuresOut, classifierHidden[1]))
 branch11:add(nn.Threshold(0, 1e-6))
+branch11:add(nn.Dropout(dropout_p))
 branch11:add(nn.Linear(classifierHidden[1], 6))
 branch11:add(nn.Softmax())
 
