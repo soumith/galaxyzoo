df2c3914781dab97b8c07003205e6bbf2f8c9bd5
Soumith Chintala <soumith@gmail.com>
2014-03-28 04:32:12 -0400

    Update dataprep.sh

--- dataprep.sh
+++ dataprep.sh
@@ -6,8 +6,8 @@ cd data
 unzip images_training_rev1.zip
 
 cd images_training_rev1
-mkdir -p ../images_training_rev1_12/
+mkdir -p ../images_training_rev1_128/
 
-find "." -name "*.jpg" | xargs -I {} echo "convert {} -resize 12x12 ../images_training_rev1_12/{}" >commands.txt
+find "." -name "*.jpg" | xargs -I {} echo "convert {} -resize 128x128 ../images_training_rev1_128/{}" >commands.txt
 cat commands.txt|parallel
 rm -f commands.txt
